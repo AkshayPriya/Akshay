18e63d355abe556e578da31c428ef2eb006e3232
AkshayPriya <100988421+AkshayPriya@users.noreply.github.com>
2022-04-19 19:53:49 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b;
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);
+  if(a>b)
+    printf("%d is largest",a);
+  else
+    printf("%d is largest",b);
+  getch();
+}
